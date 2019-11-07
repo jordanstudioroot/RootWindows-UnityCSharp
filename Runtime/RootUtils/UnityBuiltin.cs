c2b2b4d064c3d0d479689a6f8f4d119f52056f58
Jordan Nelson <jordannelson@protonmail.com>
2019-11-07 17:48:16 -0600

    remove embedded code that has been moved to a separate package, update package.json

--- Runtime/RootUtils/UnityBuiltin.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public static class UnityBuiltin {
-    public static Font Font (string fontName) {
-        return Resources.GetBuiltinResource(typeof(Font), fontName + ".ttf") as Font;
-    }
-}
